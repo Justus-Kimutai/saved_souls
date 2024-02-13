6ce30b559d0fc4fc2ee769522c2deb27e945da7e
Justus-Kimutai <123356955+Justus-Kimutai@users.noreply.github.com>
2024-02-13 07:20:57 +0300

    setup the database schema add save-soul endpoint add get-saved endpoint

--- index.js
+++ index.js
@@ -6,3 +6,71 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: "postgres",
+    // logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+  
+  sequelize
+    .sync()
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  //   model schema
+  const post = sequelize.define("post", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    village: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+  
+  app.get("/", (req, res) => {
+    res.send("Hello Captain!");
+  });
+  
+  app.post("/create-post", async (req, res) => {
+    const { name, phone, region, village } = req.body;
+    try {
+      const newPost = await post.create({ name, phone, region, village });
+      res.json(newPost);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+  app.get("/get-posts", async (req, res) => {
+    try {
+      const allPosts = await post.findAll();
+      res.json(allPosts);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
