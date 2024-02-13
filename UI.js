c72ed695b4c07f80110b2498bf2f830192e56024
Justus-Kimutai <123356955+Justus-Kimutai@users.noreply.github.com>
2024-02-13 08:52:55 +0300

    standby

--- UI.js
+++ UI.js
@@ -23,21 +23,4 @@ submitBtn.addEventListener('click',async ()=>{
       }
 })
 
-async function fetchPosts() {
-  try {
-    const response = await fetch(`${backEndURL}/get-soul`);
-    const posts = await response.json();
-    
-    const postsList = document.getElementById('postsList');
-    postsList.innerHTML = "<h2>All Posts:</h2>";
 
-    posts.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.innerHTML = `<strong>Name:</strong> ${post.name}, <strong>Phone:</strong> ${post.phone}, <strong>Region:</strong> ${post.region}, <strong>Village:</strong> ${post.village}`;
-      postsList.appendChild(postElement);
-    });
-  } catch (error) {
-    console.error(error);
-    alert('Failed to fetch posts');
-  }
-}
